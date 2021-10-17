f78f0ce3773c910a3d6414b65e16f095f0310ac9
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-10-17 12:47:44 +0600

    added SwUpdate for check for the changes and take conformation to reload to see the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,19 @@ import {SwPush, SwUpdate} from "@angular/service-worker";
 export class AppComponent  implements OnInit {
 
 
-    constructor() {
+    constructor(private swUpdate:SwUpdate) {
 
     }
 
     ngOnInit() {
+
+      if(this.swUpdate.isEnabled){
+        this.swUpdate.available.subscribe(()=>{
+           if(confirm('New version available. Locad New Version?')){
+             window.location.reload();
+           }
+        });
+      }
 
 
 
