48e9d0b4500f1e4f782a78e9469092ec0609086c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-10-20 20:25:01 +0600

    life cycle of active and Inactive in Angular

--- angular-pwa-course-1-service-workers/src/sw.js
+++ angular-pwa-course-1-service-workers/src/sw.js
@@ -1,7 +1,15 @@
-const VERSION='v1';
+const VERSION='v2';
 
 function log(messages){
     console.log(VERSION,messages);
 }
 
 log('Installing Service worker.');
+
+self.addEventListener('install',()=>{
+ log('version is installed.');
+});
+
+self.addEventListener('activate',()=>{
+    log('version is activated.');
+});
